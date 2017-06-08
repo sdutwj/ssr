69def0e71ccff2caedbf7d028b84c814f14934ca
sdutwj <1783277265@qq.com>
2017-06-08 16:34:00 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -5,4 +5,4 @@ git clone https://github.com/shadowsocksr/shadowsocksr.git
 cd shadowsocksr && ./initcfg.sh
 echo "python ~/shadowsocksr/shadowsocks/server.py -p 8989 -k wangjian -m aes-128-cfb -O auth_aes128_md5 -o tls1.2_ticket_auth_compatible -d start" >> ~/run0.sh
 echo "python ~/shadowsocksr/shadowsocks/server.py -p 8989 -k wangjian -m aes-128-cfb -O auth_aes128_md5 -o tls1.2_ticket_auth_compatible" >> ~/run1.sh
-python ~/shadowsocksr/shadowsocks/server.py -p 8989 -k wangjian -m aes-128-cfb -O auth_aes128_md5 -o tls1.2_ticket_auth_compatible -d start; /usr/sbin/sshd -D
+python ~/shadowsocksr/shadowsocks/server.py -p 8989 -k wangjian -m aes-128-cfb -O auth_aes128_md5 -o tls1.2_ticket_auth_compatible -d start
